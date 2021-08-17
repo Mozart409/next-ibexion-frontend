8dfc35ea6dce774c3071f033d2cf3fda80642d8a
Amadeus <38767929+Mozart409@users.noreply.github.com>
2021-08-17 12:54:52 +0200

    fix: :bug: changed color and removed title if no data

--- components/sections/feature-columns-group.js
+++ components/sections/feature-columns-group.js
@@ -6,7 +6,11 @@ const FeatureColumnsGroup = ({ data }) => {
       {data.features.map((feature) => (
         <div className="flex-1 text-lg" key={feature.id}>
           <CustomImage media={feature?.icon} className="w-10" />
-          <h3 className="font-bold mt-4 mb-4">{feature.title}</h3>
+          {feature.title ? (
+            <h3 className="font-bold mt-4 mb-4 text-gray-200">
+              {feature.title}
+            </h3>
+          ) : null}
           <p>{feature.description}</p>
         </div>
       ))}
