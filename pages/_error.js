5e276cd257594e3adf951ad8816b997ac11976e1
Amadeus <38767929+Mozart409@users.noreply.github.com>
2021-09-06 10:39:55 +0200

    feat: added dynamic sitemap

--- pages/_error.js
+++ pages/_error.js
@@ -4,15 +4,6 @@ function Error({ statusCode }) {
   return (
     <div>
       <div className="min-h-screen">
-        <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html:
-                'plausible("404",{ props: { path: document.location.pathname } });',
-            }}
-          ></script>
-        </Head>
-
         <h1 className="text-center text-2xl font-bold my-auto mt-24">
           {statusCode
             ? `An error ${statusCode} occurred on server`
--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const EXTERNAL_DATA_URL = 'https://beammeup.today'
+const EXTERNAL_DATA_URL = 'https://ibexion.bike'
 
 const createSitemap = (pages) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -18,7 +18,9 @@ const createSitemap = (pages) => `<?xml version="1.0" encoding="UTF-8"?>
     `
 class Sitemap extends React.Component {
   static async getInitialProps({ res }) {
-    const request = await fetch('https://cms.beammeup.today/pages')
+    const request = await fetch(
+      'https://docker-strapi-ibexion.3iondl2h16bmc.eu-central-1.cs.amazonlightsail.com/pages'
+    )
     const pages = await request.json()
 
     res.setHeader('Content-Type', 'text/xml')
