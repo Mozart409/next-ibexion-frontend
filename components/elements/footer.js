c1c27d6a061fcb037f84c564bc37d190ce13c024
Amadeus <38767929+Mozart409@users.noreply.github.com>
2021-11-05 11:01:34 +0100

    feat: :sparkles: added divider

--- components/elements/footer.js
+++ components/elements/footer.js
@@ -17,25 +17,29 @@ const Footer = ({ footer }) => {
             />
           )}
         </div>
-        <nav className="flex flex-row flex-wrap items-start mb-10 lg:gap-20 lg:justify-end divide-y-4 divide-gecko-gray divide-solid">
+        <nav className="flex flex-row flex-wrap items-start mb-10 lg:gap-20 lg:justify-end">
           {footer.columns.map((footerColumn) => (
             <div
               key={footerColumn.id}
-              className="mt-10 w-6/12 lg:mt-0 lg:w-auto"
+              className="mt-10 w-6/12 lg:mt-0 lg:w-auto "
             >
               <p className="font-semibold tracking-wide text-gray-300 uppercase">
                 {footerColumn.title}
               </p>
-              <ul className="mt-2">
-                {footerColumn.links.map((link) => (
-                  <li
-                    key={link.id}
-                    className="py-1 px-1 -mx-1 text-gray-400 hover:text-gray-600"
-                  >
-                    <CustomLink link={link}>{link?.text}</CustomLink>
-                  </li>
-                ))}
-              </ul>
+              <div className="flex flex-col">
+                <ul className="mt-2 divide-y-2 divide-gecko-gray divide-solid md:divide-y-0">
+                  {footerColumn.links.map((link) => (
+                    <li
+                      key={link.id}
+                      className="py-1 px-1 -mx-1 text-gray-400 hover:text-gray-600 "
+                    >
+                      <CustomLink className="" link={link}>
+                        {link?.text}
+                      </CustomLink>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           ))}
         </nav>
