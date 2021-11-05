15a2f169ad15a0c3f4c6482f90a65022ae4cafb8
Amadeus <38767929+Mozart409@users.noreply.github.com>
2021-11-05 12:11:39 +0100

    fix: :bug: throws when no image is found

--- components/elements/image.tsx
+++ components/elements/image.tsx
@@ -11,19 +11,22 @@ interface CustomImageProps {
 const CustomImage = ({ media, className, width, height }: CustomImageProps) => {
   const fullUrl = getStrapiMedia(media.url)
 
-  return (
-    <div>
-      <Image
-        src={fullUrl}
-        alt={media?.alternativeText || ''}
-        title={media?.caption || ''}
-        layout="intrinsic"
-        className={className}
-        width={width || media?.width}
-        height={height || media?.height}
-      />
-    </div>
-  )
+  if (media)
+    return (
+      <div>
+        <Image
+          src={fullUrl}
+          alt={media?.alternativeText || ''}
+          title={media?.caption || ''}
+          layout="intrinsic"
+          className={className}
+          width={width || media?.width}
+          height={height || media?.height}
+        />
+      </div>
+    )
+
+  return <></>
 }
 
 export default CustomImage
