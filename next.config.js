080d7780f1993aaf3b306162bd7e2a163917eb51
Amadeus <38767929+Mozart409@users.noreply.github.com>
2021-11-09 18:48:06 +0100

    feat: :iphone: changed next config

--- next.config.js
+++ next.config.js
@@ -13,9 +13,6 @@ let nextConfig = {
 
   webpack5: true,
   images: {
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [4, 8, 16, 32, 48, 64, 96, 128, 256, 384],
     domains: [
       'localhost',
       'cms.ibexion.de',
